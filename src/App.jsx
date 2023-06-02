b01f678b822bd7c6be55d86b42f79e0aea2ac543
FocusSTmatt <113565590+FocusSTmatt@users.noreply.github.com>
2023-06-02 16:10:04 -0700

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ const [board, setBoard] = useState(["", "", "", "", "", "", "", "", "",]);
+
+  return (
+    <>
+    <h1>test</h1>
+        
+    </>
+  )
+}
+
+export default App
